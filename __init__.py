07e8ce0509da402a7753e550bc886d7d60fa7694
Alexey-t <support@uvviewsoft.com>
2017-03-07 19:00:29 +0300

    refc

--- __init__.py
+++ __init__.py
@@ -1,19 +1,21 @@
 from cudatext import *
 
-MAX_LEN=50*1024
+MIN_LEN = 1
+MAX_LEN = 50*1024
+MAX_SHOW = 80
+PRE = '[Auto-Copy] '
 
 class Command:
     def on_caret(self, ed_self):
         carets = ed_self.get_carets()
         if len(carets)>1: return
-        x,y,x2,y2 = carets[0]
-        if y2!=-1:
-            if (y2,x2)<(y,x):
-                x,y,x2,y2 = x2,y2,x,y
-            text = ed_self.get_text_substr(x,y,x2,y2)
-            if len(text)>MAX_LEN:
-                msg_status('[Auto-copy to clipboard] Cannot copy such big block')
-                return
-            app_proc(PROC_SET_CLIP, text)
-            msg_status('[Auto-copy to clipboard] Copied "%s"'%(text))
 
+        s = ed_self.get_text_sel()
+        if not s: return
+        if len(s)<MIN_LEN: return
+        if len(s)>MAX_LEN: return msg_status(PRE+'Too big block')
+        app_proc(PROC_SET_CLIP, s)
+
+        if len(s)>MAX_SHOW:
+            s = s[:MAX_SHOW]+'...'
+        msg_status(PRE+'Copied "%s"'%s)
---
+++ readme/readme.txt
@@ -0,0 +1,10 @@
+plugin for CudaText.
+on selection (with mouse, shift+arrows, ctrl+A, etc), copies selected block to clipboard.
+
+wont work with multi-carets.
+wont work for too big blocks (default is 50K, you can change constant in source).
+wont handle vertical blocks.
+handles forward/backward selections.
+
+author: Alexey (CudaText)
+license: MIT
