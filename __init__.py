b79fce3846f9120c8a0b9fec228c2c7a2aa392e3
Alexey-t <support@uvviewsoft.com>
2017-03-09 22:11:53 +0300

    more opts

--- __init__.py
+++ __init__.py
@@ -1,9 +1,20 @@
 from cudatext import *
 
+#Linux gtk2 has 3 buffers:
+#  a) clipboard, b) primary selection, c) secondary selection
+#You can auto-copy to 1st, 2nd or 1st+2nd
+TO_CLIPBOARD = False #True
+TO_PRIMARY_SEL = True
+
+#misc options
 MIN_LEN = 1
 MAX_LEN = 50*1024
 MAX_SHOW = 80
-PRE = '[Auto-Copy] '
+PREFIX = '[Auto-Copy] '
+
+if app_api_version()<'1.0.170':
+    msg_box('Auto-Copy plugin needs newer app version', MB_OK+MB_ICONERROR)
+
 
 class Command:
     def on_caret(self, ed_self):
@@ -13,9 +24,14 @@ class Command:
         s = ed_self.get_text_sel()
         if not s: return
         if len(s)<MIN_LEN: return
-        if len(s)>MAX_LEN: return msg_status(PRE+'Too big block')
-        app_proc(PROC_SET_CLIP, s)
+        if len(s)>MAX_LEN: return msg_status(PREFIX+'Too big block')
+
+        if TO_CLIPBOARD:
+            app_proc(PROC_SET_CLIP, s)
+        if TO_PRIMARY_SEL:
+            app_proc(PROC_SET_CLIP_ALT, s)
 
         if len(s)>MAX_SHOW:
             s = s[:MAX_SHOW]+'...'
-        msg_status(PRE+'Copied "%s"'%s)
+        kind = '[main/primary] ' if TO_CLIPBOARD and TO_PRIMARY_SEL else '[main] ' if TO_CLIPBOARD else '[primary] ' if TO_PRIMARY_SEL else ' '
+        msg_status(PREFIX+kind+'Copied "%s"'%s)
--- readme/readme.txt
+++ readme/readme.txt
@@ -6,5 +6,13 @@ wont work for too big blocks (default is 50K, you can change constant in source)
 wont handle vertical blocks.
 handles forward/backward selections.
 
+
+plugin has options inside __init__.py file
+(if boolean, True means "on", False means "off"):
+  - TO_CLIPBOARD - auto-copy to usual clipboard (exists in every OS)
+  - TO_PRIMARY_SEL - auto-copy to primary-selection (alternate buffer on Linux gtk2, it's used by CudaText on middle-button-click, and by other Linux editors too.)
+if you want to disable auto-copy to usual clipboard, set TO_CLIPBOARD to False. this is good, if you don't want to overwrite usual clipboard with Auto-Copy, and want Auto-Copy set only primary-sel buffer. then middle-click-paste will paste from primary-sel buffer, and it's like in Linux apps.
+
+
 author: Alexey (CudaText)
 license: MIT
